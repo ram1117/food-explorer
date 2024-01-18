a4fb825287430fa91569c2c66f784fbf22702bbc
ram1117 <ramkumark1@outlook.com>
2024-01-19 01:15:33 +0530

    make styling change to navigation menu

--- src/components/header/NavList.tsx
+++ src/components/header/NavList.tsx
@@ -1,24 +1,25 @@
-'use client';
+"use client";
 
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const NavList = () => {
   const activePath = usePathname();
-  const activeStyle = 'gradient-text-primary font-semibold hover:drop-shadow-xl';
+  const activeStyle =
+    "gradient-text-primary font-semibold hover:drop-shadow-xl";
   return (
-    <nav className='navlist'>
+    <nav className="navlist">
       <ul className="flex gap-2 md:gap-6 lg:gap-12 items-center font-semibold">
         <li
           className={`text-sm  md:text-lg ${
-            activePath === '/meals' ? activeStyle : ''
+            activePath === "/meals" ? activeStyle : ""
           }`}
         >
-          <Link href="/meals">Meals</Link>
+          <Link href="/meals">Recipes</Link>
         </li>
         <li
           className={`text-sm  md:text-lg ${
-            activePath === '/community' ? activeStyle : ''
+            activePath === "/community" ? activeStyle : ""
           }`}
         >
           <Link href="/community">Community</Link>
